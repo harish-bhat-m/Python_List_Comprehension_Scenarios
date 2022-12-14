eef6706b06599b3ff68f75e732d5110794bb88c7
harish-bhat-m <harish.bhat.m@gmail.com>
2022-12-14 21:11:42 +0530

    Update

---
+++ code_examples/Example3.py
@@ -0,0 +1,6 @@
+even = [number for number in range(10) if number % 2 == 0]
+odd = [number for number in range(10) if number % 2 != 0]
+print("The set of odd numbers between 0 to 10")
+print(odd)
+print("The set of even numbers between 0 to 10")
+print(even)
